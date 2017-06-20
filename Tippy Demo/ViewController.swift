d6cc413b8bd1e2c43e1089ebe3576fadfd29a275
Nicolas Rodriguez <nhr@fb.com>
2017-06-20 15:34:31 -0700

    Tip and total functionality added. Values change depending on amount value and tip selected

--- Tippy Demo/ViewController.swift
+++ Tippy Demo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,5 +29,15 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+    }
 }
 
